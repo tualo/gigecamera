31b072eaaab546c3d6e3635cf792a546ee6aaf24
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2015-04-14 16:19:33 +0200

    added genapi test programm

---
+++ test/genapi.js
@@ -0,0 +1,16 @@
+
+var GenAPI = require("../lib/classes/genapi/GenAPI").GenAPI;
+var fs = require('fs');
+var path = require('path');
+
+
+var genapi = new GenAPI();
+genapi.on('ready',function(){
+  //console.log(genapi.values);
+  for(var name in genapi.values){
+    if (typeof genapi.values[name].address!=='undefined'){
+      console.log(genapi.values[name].typeName ,genapi.values[name].address,'with length',genapi.values[name].length,'is ready for reading');
+    }
+  }
+})
+genapi.setDefinitionFile(path.join(__dirname,'..','Basler_Racer_GigE_999718b3_Version_3_1.xml'));
